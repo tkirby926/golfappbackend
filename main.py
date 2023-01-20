6df105500ee694960f608baf5f9e23229ef70e90
tkirby926 <109627147+tkirby926@users.noreply.github.com>
2023-01-20 16:49:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import create_app
-from views import create_server_connection, run_query, getThreeWeeks
+from golfappbackend import create_app
+from golfappbackend.views import create_server_connection, run_query, getThreeWeeks
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import flask
