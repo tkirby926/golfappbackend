8cc6db35b7d37e76772137180417a9a4b425a727
Thomas Kirby <tmkirby@umich.edu>
2023-01-23 20:25:21 -0500

    change

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 
-from audioop import reverse
-from crypt import methods
-from email.policy import default
-from os import curdir
-from re import I
-from flask import Blueprint, render_template, request
+# from audioop import reverse
+# from crypt import methods
+# from email.policy import default
+# from os import curdir
+# from re import I
+# from flask import Blueprint, render_template, request
 import flask
-import mysql.connector
-from mysql.connector import Error
-from numpy import mat
-import pandas as pd
-import pgeocode
-from haversine import haversine, Unit
-import random
-from PIL import Image
-import json
-import base64
-import requests
-import io
-import uuid
-import hashlib
-import stripe
-import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.interval import IntervalTrigger
-from apscheduler.triggers.cron import CronTrigger
-from apscheduler.schedulers.background import BackgroundScheduler
-import random, string
+# import mysql.connector
+# from mysql.connector import Error
+# from numpy import mat
+# import pandas as pd
+# import pgeocode
+# from haversine import haversine, Unit
+# import random
+# from PIL import Image
+# import json
+# import base64
+# import requests
+# import io
+# import uuid
+# import hashlib
+# import stripe
+# import datetime
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.triggers.interval import IntervalTrigger
+# from apscheduler.triggers.cron import CronTrigger
+# from apscheduler.schedulers.background import BackgroundScheduler
+# import random, string
 
 
 # BUCKET = 'golftribephotos'
