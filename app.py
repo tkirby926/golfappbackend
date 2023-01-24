b900c1a7515b0a248e031cdf94c172e9494e17f7
Thomas Kirby <tmkirby@umich.edu>
2023-01-23 22:23:53 -0500

    change

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ BUCKET = 'golftribephotos'
 
 def create_tables():
     connection = create_server_connection()
-    cursor = run_query(connection, """CREATE TABLE teetimes (
+    cursor = run_query_basic(connection, """CREATE TABLE teetimes (
         timeid int NOT NULL AUTO_INCREMENT,
         teetime datetime DEFAULT NULL,
         uniqid varchar(20) DEFAULT NULL,
@@ -31,7 +31,7 @@ def create_tables():
         PRIMARY KEY (timeid),
         KEY uniqid (uniqid)
         )""")
-    cursor = run_query(connection, """CREATE TABLE users (
+    cursor = run_query_basic(connection, """CREATE TABLE users (
         username varchar(20) NOT NULL,
         firstname varchar(30) DEFAULT NULL,
         lastname varchar(30) DEFAULT NULL,
@@ -50,26 +50,26 @@ def create_tables():
         PRIMARY KEY (username)
         )""")
 
-    cursor = run_query(connection, """CREATE TABLE admins (
+    cursor = run_query_basic(connection, """CREATE TABLE admins (
         username varchar(20) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE bookedtimes (
+    cursor = run_query_basic(connection, """CREATE TABLE bookedtimes (
         username varchar(20) DEFAULT NULL,
         timeid varchar(20) DEFAULT NULL,
         KEY username (username),
         KEY timeid (timeid),
         CONSTRAINT bookedtimes_ibfk_1 FOREIGN KEY (username) REFERENCES USERS (username)
         )""")
-    cursor = run_query(connection, """CREATE TABLE cookies (
+    cursor = run_query_basic(connection, """CREATE TABLE cookies (
         username varchar(20) DEFAULT NULL,
         sessionid varchar(32) DEFAULT NULL,
         user char(1) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE coursecloseddates (
+    cursor = run_query_basic(connection, """CREATE TABLE coursecloseddates (
         date date DEFAULT NULL,
         uniqid int DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE coursereviews (
+    cursor = run_query_basic(connection, """CREATE TABLE coursereviews (
         uniqid int DEFAULT NULL,
         rating int DEFAULT NULL,
         description varchar(300) DEFAULT NULL,
@@ -77,7 +77,7 @@ def create_tables():
         username varchar(20) DEFAULT NULL
         )""") 
 
-    cursor = run_query(connection, """CREATE TABLE courses (
+    cursor = run_query_basic(connection, """CREATE TABLE courses (
         uniqid int NOT NULL AUTO_INCREMENT,
         latitude varchar(20) DEFAULT NULL,
         longitude varchar(20) DEFAULT NULL,
@@ -94,22 +94,22 @@ def create_tables():
         auth char(1) DEFAULT NULL,
         PRIMARY KEY (uniqid)
         )""")  
-    cursor = run_query(connection, """CREATE TABLE emailverif (
+    cursor = run_query_basic(connection, """CREATE TABLE emailverif (
         username varchar(20) DEFAULT NULL,
         emailcode varchar(16) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE friendships (
+    cursor = run_query_basic(connection, """CREATE TABLE friendships (
         UserId1 varchar(20) DEFAULT NULL,
         UserId2 varchar(20) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE ledger (
+    cursor = run_query_basic(connection, """CREATE TABLE ledger (
         timeid varchar(20) DEFAULT NULL,
         uniqid varchar(20) DEFAULT NULL,
         Cost varchar(20) DEFAULT NULL,
         user varchar(20) DEFAULT NULL,
         timestamp datetime DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE messages (
+    cursor = run_query_basic(connection, """CREATE TABLE messages (
         content varchar(700) DEFAULT NULL,
         userid1 varchar(20) DEFAULT NULL,
         userid2 varchar(20) DEFAULT NULL,
@@ -117,23 +117,23 @@ def create_tables():
         messageid int NOT NULL AUTO_INCREMENT,
         PRIMARY KEY (messageid)
         )""")
-    cursor = run_query(connection, """CREATE TABLE posts (
+    cursor = run_query_basic(connection, """CREATE TABLE posts (
         content varchar(700) DEFAULT NULL,
         username varchar(20) DEFAULT NULL,
         timestamp datetime DEFAULT NULL,
         link varchar(20) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE requestedfriends (
+    cursor = run_query_basic(connection, """CREATE TABLE requestedfriends (
         username1 varchar(20) DEFAULT NULL,
         username2 varchar(20) DEFAULT NULL
         )""")
-    cursor = run_query(connection, """CREATE TABLE reviews (
+    cursor = run_query_basic(connection, """CREATE TABLE reviews (
         username varchar(20) DEFAULT NULL,
         content varchar(400) DEFAULT NULL,
         timestamp datetime DEFAULT NULL,
         rating int DEFAULT NULL
         )""")
-    cursor = run_query(connection, """ CREATE TABLE teetimeschedule (
+    cursor = run_query_basic(connection, """ CREATE TABLE teetimeschedule (
         course_id int DEFAULT NULL,
         days text,
         time time DEFAULT NULL,
