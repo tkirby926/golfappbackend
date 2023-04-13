ce642f2bde06da7d50ec0faf3a44926a6b3be302
tkirby926 <109627147+tkirby926@users.noreply.github.com>
2023-04-13 12:12:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1091,7 +1091,7 @@ def validate_user(username, password):
     print(correct_login)
     context = flask.jsonify({'is_user': is_user, 'correct_login': correct_login, 'too_many_attmpts': False, 'cookie': cookie})
     context.set_cookie('username', cookie)
-    context = make_response(context)
+    context = flask.make_response(context)
     context.headers['Access-Control-Allow-Credentials'] = 'true'
     print(context)
     return context
