c21a8b1b952525f2ddb45bda06c8ebe2835151a9
Thomas Kirby <tmkirby@umich.edu>
2023-04-18 10:40:02 -0400

    change

--- app.py
+++ app.py
@@ -876,7 +876,7 @@ def get_all_posts(page):
     if user == False:
         context = {'not_user': True}
         return flask.jsonify(**context)
-    cursor = run_query(connection, "SELECT * FROM Posts WHERE username = %s OR username IN (SELECT U.username FROM USERS U, Friendships F WHERE ((F.userid2 = " +
+    cursor = run_query(connection, "SELECT P.content, P.username, P.timestamp, P.link, U.imageurl FROM Posts P, Users U WHERE P.username = U.username AND P.username = %s OR P.username IN (SELECT U.username FROM USERS U, Friendships F WHERE ((F.userid2 = " +
                                     "%s AND U.Username = F.userid1) OR (F.userid1 = %s AND U.Username = F.userid2))) ORDER BY timestamp DESC LIMIT 6 OFFSET %s;", (user, user, user, page * 5))
     posts = cursor.fetchall()
     print('hello')
