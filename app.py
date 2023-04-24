43bf43ebf6595dc3ffee3511b13a8a1e0b75a7f8
Thomas Kirby <tmkirby@umich.edu>
2023-04-24 11:56:30 -0400

    change

--- app.py
+++ app.py
@@ -1449,7 +1449,7 @@ def get_single_user():
     if username == False:
         context = {'user': False}
         return flask.jsonify(**context)
-    cursor = run_query(connection, "SELECT username, firstname, lastname, email, score, favcourse, drinking, music, favgolf, favteam, college, playstyle, descript, wager, cart, imageurl FROM USERS WHERE username = %s;", (username, ))
+    cursor = run_query(connection, "SELECT username, firstname, lastname, email, score, favcourse, drinking, music, favgolf, favteam, college, playstyle, descript, wager, cart, imageurl, age FROM USERS WHERE username = %s;", (username, ))
     return flask.jsonify({'user': cursor.fetchone()})
 
 @app.route('/api/v1/edit', methods=["PUT"])
