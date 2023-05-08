0eed74268477f02a5d36eb18af6a3315bbff22c9
Thomas Kirby <tmkirby@umich.edu>
2023-05-08 18:24:52 -0400

    change

--- app.py
+++ app.py
@@ -1213,7 +1213,7 @@ def get_courses_times(courseid, date):
     connection = create_server_connection()
     cursor = run_query(connection, "SELECT coursename, imageurl, street, town, state, zip FROM COURSES WHERE uniqid = %s;", (courseid, ))
     course_info = cursor.fetchone()
-    cursor = run_query(connection, "SELECT * FROM TEETIMES T, LEDGER B WHERE B.timeid = T.timeid AND (4 - SUM(B.numusers)) != 0 AND T.uniqid = %s" + 
+    cursor = run_query(connection, "SELECT T.timeid, teetime, uniqid, cost, spots, cart, holes, SUM(B.numusers) AS X FROM TEETIMES T, LEDGER B WHERE B.timeid = T.timeid AND X < 4 AND T.uniqid = %s" + 
     " AND CAST(T.teetime AS DATE) = %s ORDER BY T.teetime;", (courseid, date))
     course_times = cursor.fetchall()
     context = {'course_info': course_info, 'course_times': course_times}
